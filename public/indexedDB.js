344b46f019b959970613b9318263d55b96c79625
Martin Renteria <renteriamp96@gmail.com>
2021-02-15 17:43:59 -0800

    continuing to add on to indexedDb file to set-up indexedDB to app functionality

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -8,3 +8,26 @@ request.onupgradeneeded = (e) => {
 	db.createObjectStore("transaction", { autoIncrement: true });
 };
 
+request.onsuccess = (e) => {
+	db = e.target.result;
+
+	//Checks to see if app is online before checking database.
+	if (navigator.onLine) {
+		checkDatabase();
+	}
+};
+
+request.onerror = (e) => {
+	console.log(`Error ${e.target.errorCode}`);
+};
+
+const saveTransaction = (data) => {
+	// Create a transaction on the pending db with readwrite access
+	const transaction = db.transaction([ "transaction" ], "readwrite");
+
+	// Access your object store
+	const store = transaction.objectStore("transaction");
+
+	// Add data to your store with add method.
+	store.add(data);
+};
