85e7780159bc34f5c61c3ac72a3cf5b8db7962ca
Martin Renteria <renteriamp96@gmail.com>
2021-02-19 15:48:23 -0800

    Adding additional files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,6 +6,8 @@ const FILES_TO_CACHE = [
 	"/index.html",
 	"/manifest.webmanifest",
 	"/styles.css",
+    '/index.js',
+    '/indexDB.js',
 	"/icons/icon-192x192.png",
 	"/icons/icon-512x512.png"
 ];
