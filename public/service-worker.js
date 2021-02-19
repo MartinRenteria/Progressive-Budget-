4f0c88ac1dae0417bed8130a2b0d0514e42e1245
Martin Renteria <renteriamp96@gmail.com>
2021-02-19 15:00:34 -0800

    Adding initial installation to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,23 @@
 const STATIC_BUDGET = "static-budget-v1";
 const DATA_BUDGET = "data-budget-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/manifest.webmanifest",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+  ];
+
+  // Adds data to cache using an async function
+self.addEventListener("install", async (e) => {
+    const budgetData = await caches.open(BUDGET_DATA);
+    await budgetData.add("/api/transaction");
+  
+    const staticBudget = await caches.open(STATIC_BUDGET);
+    await staticBudget.addAll(FILES_TO_CACHE);
+  
+    self.skipWaiting();
+  });
+  
